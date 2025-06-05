22c443a940d0a584ff7ab2bdac5fe3b015877214
setti-Aswini <settiaswini004@gmail.com>
2025-06-05 23:18:53 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const value = btn.textContent;
+    if (value === "C") {
+      display.value = "";
+    } else if (value === "=") {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = "Error";
+      }
+    } else {
+      display.value += value;
+    }
+  });
+});
